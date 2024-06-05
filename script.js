e3034946324b4453ece26413c4873e06aae83093
Jhon Fredy <fredybencar22@gmail.com>
2024-06-05 14:16:00 -0500

    solucion workshop jhon betancur

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 let resultado= 5 + 9;
 console.log(resultado);
 
+let p, q;
+
+// primera expresión  (a)
+console.log("p q NOT p NOT p AND q");
+p = true; q = true;
+console.log(p, q, !p, !q, !(p && q));
+p = true; q = false;
+console.log(p, q, !p, !q, !(p && q));
+p = false; q = true;
+console.log(p, q, !p, !q, !(p && q));
+p = false; q = false;
+console.log(p, q, !p, !q, !(p && q));
+
+// segunda expresión (b)
+console.log("p q NOT p NOT q NOT p AND NOT q");
+p = true; q = true;
+console.log(p, q, !p, !q, !(p && !q));
+p = true; q = false;
+console.log(p, q, !p, !q, !(!p && q));
+p = false; q = true;
+console.log(p, q, !p, !q, !(!p && !q));
+p = false; q = false;
+console.log(p, q, !p, !q, !(p && q));
--- workshop.js
+++ workshop.js
@@ -89,3 +89,31 @@ console.log("Resultado c:", resultadoC);
 // Operación d
  resultadoD = (x / y) / (z + w);
 console.log("Resultado d:", resultadoD);
+
+// --------------------------------------------------------------------------------------------
+
+// solucion punto 3
+
+let p, q;
+
+//  primera expresión (a)
+console.log("p q NOT p NOT p AND q");
+p = true; q = true;
+console.log(p, q, !p, !q, !(p && q));
+p = true; q = false;
+console.log(p, q, !p, !q, !(p && q));
+p = false; q = true;
+console.log(p, q, !p, !q, !(p && q));
+p = false; q = false;
+console.log(p, q, !p, !q, !(p && q));
+
+// segunda expresión  (b)
+console.log("p q NOT p NOT q NOT p AND NOT q");
+p = true; q = true;
+console.log(p, q, !p, !q, !(p && !q));
+p = true; q = false;
+console.log(p, q, !p, !q, !(!p && q));
+p = false; q = true;
+console.log(p, q, !p, !q, !(!p && !q));
+p = false; q = false;
+console.log(p, q, !p, !q, !(p && q));
